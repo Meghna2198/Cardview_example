241c030656116fdaf0949597ca1f8008517f90b3
Meghna.2198 <meghupanwar98@gmail.com>
2019-04-21 18:03:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cardview_example/UserViewHolderImpl.java
@@ -0,0 +1,9 @@
+package com.example.cardview_example;
+
+import android.view.View;
+
+class UserViewHolderImpl extends AllUsersAdapter.UserViewHolder {
+    public UserViewHolderImpl(View itemView) {
+        super(itemView);
+    }
+}
